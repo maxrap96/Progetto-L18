17251e80e22eff6383c47d1a549976ad53955439
AndreaLucenti <32876099+AndreaLucenti@users.noreply.github.com>
2018-04-06 16:03:52 +0200

    Coins
    Coins con array

--- src/Distributore/Coins.java
+++ src/Distributore/Coins.java
@@ -3,20 +3,20 @@ package Distributore;
 import static java.lang.Integer.parseInt;
 
 public class Coins {
-    private int cent5, cent10, cent20, cent50, euro1, euro2;
+    private int money[] = new int[6];
     private double balance, profit;
     private double credit;
     private boolean changeAvailbe;
     private String[] addedCoins;
 
     public Coins() {
-        this.cent5 = 20;
-        this.cent10 = 30;
-        this.cent20 = 20;
-        this.cent50 = 10;
-        this.euro1 = 5;
-        this.euro2 = 5;
-        this.balance = cent5*0.05 + cent10*0.10 + cent20*0.2 + cent50*0.50 + euro1*1 + euro2*2;
+        this.money[0] = 20;
+        this.money[1] = 30;
+        this.money[2] = 20;
+        this.money[3] = 10;
+        this.money[4] = 5;
+        this.money[5] = 5;
+        this.balance = money[0]*0.05 + money[1]*0.10 + money[2]*0.2 + money[3]*0.50 + money[4]*1 + money[5]*2;
         this.credit = 0;
         this.profit = 0;
         this.changeAvailbe = true;
@@ -43,12 +43,9 @@ public class Coins {
                     parseInt(addedCoins[3]) * 0.50 + parseInt(addedCoins[4]) * 1 + parseInt(addedCoins[5]) * 2;
 
             //Utilizzo le motenete inserite al credit per dare resto
-            cent5 += parseInt(addedCoins[0]);
-            cent10 += parseInt(addedCoins[1]);
-            cent20 += parseInt(addedCoins[2]);
-            cent50 += parseInt(addedCoins[3]);
-            euro1 += parseInt(addedCoins[4]);
-            euro2 += parseInt(addedCoins[5]);
+           for(int i = 0; i < 6; i++){
+               money[i] += parseInt(addedCoins[i]);
+           }
 
         } // Significa che ho inserito tutti i dati riferiti ai singoli tagli.
         else {
@@ -61,24 +58,47 @@ public class Coins {
     }
 
 
-    //TODO funzione da fare per dare il resto nel minor numero di monete se e solo se non ho esaurito
-    //TODO alcun tipo di monete
     public void giveChange() {
-
 
         //Basterebbe un if(checkChange()) che restituisca true o false
         // nel caso sia possibile o meno erogare il resto. Quella funzione aggiorna changeAvaible così è possibile
         // tenere traccia della possibilità di dare resto. Può essere utile anche per le interfacce
 
             if(checkChange()){
-                int Rcent5, Rcent10, Rcent20, Rcent50, Reuro1, Reuro2; //(Rcent5, Rcent10, ecc...) è il numero di monete
-                double resto;                                          // del tipo indicato
+                int[] change = new int[6];                        //(change[0], change[1], ecc...) è il numero di monete
+                int [] divisor = {5,10,20,50,100,200};            // del tipo indicato
+                double resto;
 
-                Reuro2 =(int)(credit*100)/200;
-                resto = (credit*100) % 200;
+                resto = (credit*100);
+
+                for(int i = 5; i > -1; i--){
+                    change[i] = (int)(resto)/ divisor[i];
+                    resto = resto % divisor[i];
+
+                    if(change[i] > money[i]){
+                        change[i] = money[i];
+                        resto += (change[i] - money[i])* divisor[i];
+                    }
+                    money[i] -= change[i];
+                }
+
+                if(credit > balance){
+                    double abb = credit-balance;
+                    System.out.println("Resto NON erogabile pari a: "+(float)abb);
+                }
+
+                //TODO Abbellire output
+                System.out.println("5c:" + change[0] + "\n10c:" + change[1] + "\n20c:" + change[2] + "\n50c:"
+                        + change[3] + "\n1E:" +change[4] + "\n2E:" + change[5]);
 
 
-                if(Reuro2 > euro2){
+
+                //TODO Decidere se va bene l'utilizzo di array oppure tenere il metodo vecchio
+                /*
+                Reuro2 = (int)credit*100/200;
+                    resto = credit % 200;
+
+                    if(Reuro2 > euro2){
                     resto += (Reuro2-euro2)*200;
                     Reuro2 = euro2;
                 }
@@ -144,15 +164,18 @@ public class Coins {
                 // s....out.println(resto). Si possono accodare caratteri alle stringhe senza troppi fastidi (?)
 
                 System.out.println("5c:" + Rcent5 + "\n10c:" + Rcent10 + "\n20c:" + Rcent20 + "\n50c:" + Rcent50
-                        + "\n1E:" + Reuro1 + "\n2E:" + Reuro2);
+                        + "\n1E:" + Reuro1 + "\n2E:" + Reuro2); */
             }
+
     }
 
     public boolean checkChange(){
-        if(cent5 != 0 && cent10 != 0 && cent20 != 0 && cent50 != 0 && euro1 != 0 && euro2 != 0){
-            return true;
+        for(int i= 0; i <6; i++) {
+            if (money[i] == 0) {
+                return false;
+            }
         }
-        return false;
+        return true;
     }
 }
 
