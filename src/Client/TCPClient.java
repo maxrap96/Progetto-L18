b2cca05a84f231d6ea31531e94040c7767061d13
DarioMaestri <32876188+Dareus1996@users.noreply.github.com>
2018-04-14 15:12:37 +0200

    Aggiornamento Server - Client, ora mando il File

--- src/Client/TCPClient.java
+++ src/Client/TCPClient.java
@@ -9,18 +9,38 @@ import java.net.*;
 
 public class TCPClient {
     public static void main(String argv[]) throws Exception {
-        String sentence;
+
+        File file = new File("src/File_Testo/menu.txt");
+        String sentece;
         String modifiedSentence;
-        BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 
         Socket clientSocket = new Socket("localhost", 2222);
+        PrintWriter out =
+                new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in =
+                new BufferedReader(
+                        new InputStreamReader(clientSocket.getInputStream()));
+        BufferedReader stdIn =
+                new BufferedReader(
+                        new FileReader(file));
+
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+
         BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
-        sentence = inFromUser.readLine();
-        outToServer.writeBytes(sentence + '\n');
+        while ((sentece = stdIn.readLine()) != null) {
+            out.println(sentece);
+            System.out.println("echo: " + in.readLine());
+        }
+
+        /*outToServer.writeBytes(sentece + "\n");
         modifiedSentence = inFromServer.readLine();
-        System.out.println(modifiedSentence);
+        System.out.println(modifiedSentence);*/
+
+
         clientSocket.close();
+        stdIn.close();
+        inFromServer.close();
+        outToServer.close();
     }
 }
