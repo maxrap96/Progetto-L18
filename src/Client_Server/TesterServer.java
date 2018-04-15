27babfe0f5c6b9038522cddaf80d3b6b9b43b8a4
DarioMaestri <32876188+Dareus1996@users.noreply.github.com>
2018-04-15 18:37:43 +0200

    Rifinitura invio stats.txt a Server

--- src/Client_Server/TesterServer.java
+++ src/Client_Server/TesterServer.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class TesterServer {
+
     public static void main(String[] args) throws IOException{
         File fileStats =
                 new File("src/Client_Server/statsServer.txt");
@@ -18,12 +19,16 @@ public class TesterServer {
             BufferedReader inFromClient =
                     new BufferedReader(
                             new InputStreamReader(clientSocket.getInputStream())); // Oggetto per leggere da Client
+            PrintWriter empyFile =
+                    new PrintWriter(fileStats.getPath());
+            empyFile.write(""); // Svuoto il precedente file
+            empyFile.close();
 
             String stringFromClient;
             while ((stringFromClient = inFromClient.readLine()) != null) {
                 FileOutputStream fileOutputStream =
-                        new FileOutputStream(fileStats.getPath());
-                fileOutputStream.write(stringFromClient.getBytes());
+                        new FileOutputStream(fileStats.getPath(), true); // Scrivo il file
+                fileOutputStream.write((stringFromClient + "\n").getBytes());
                 outToClient.println(stringFromClient + " Server");  // Reinvio di file, questo caso é un esempio per
                                                                     // vedere se funziona
             }
