99511c484d61420ef7ce1f295840bb4e789cf707
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-04-15 10:26:21 +0200

    Credito non funzionante

--- src/Distributore/InterfacciaDistributore/ListenerTry.java
+++ src/Distributore/InterfacciaDistributore/ListenerTry.java
@@ -7,7 +7,8 @@ import java.awt.event.ActionListener;
 public class ListenerTry implements ActionListener {
 
     private JTextArea jTextArea;
-    private double credit = 0.0;
+   // private double credit[] = {0, 0 , 0, 0, 0, 0};
+    private double credito = 0;
     private final String monete[] = {"0.05", "0.10", "0.20", "0.50", "1", "2"};
     private final String bevande[] = {"Caffè Espresso", "Cappuccino", "Tè", "Caffè Corretto", "Caffè Lungo",
             "Ginseng", "Bicchiere"};
@@ -24,22 +25,25 @@ public class ListenerTry implements ActionListener {
         // Stampa nome e prezzo della bevanda selezionata e il credito disponibile
         for (int j = 0; j < bevande.length; j++) {
             if (stringaPulsante.equals(bevande[j])) {
-                jTextArea.setText(bevande[j].toUpperCase() + "\n" + "COSTO: " + costo[j] + "\n\n\n" + "CREDITO: " + credit);
+                jTextArea.setText(bevande[j].toUpperCase() + "\n" + "COSTO: " + costo[j] + "\n\n\n" + "CREDITO: " + credito);
             }
         }
         if (stringaPulsante.equals("")) {
-            jTextArea.setText("BEVANDA NON DISPONIBILE" + "\n\n\n\n" + "CREDITO:");
+            jTextArea.setText("BEVANDA NON DISPONIBILE" + "\n\n\n\n" + "CREDITO: " + credito);
         }
 
-        // Stampa il credito disponibile dopo avers inserito una moneta
-       /* for (int i = 0; i < monete.length; i++) {
+        // Stampa il credito disponibile dopo aver inserito una moneta
+        for (int i = 0; i < monete.length; i++) {
             if (stringaPulsante.equals(monete[i])) {
-                credit += Double.parseDouble(monete[i]);
-
+                credito += Double.parseDouble(monete[i]);
+                //jTextArea.setText("INSERITI: " + "\n" + "COSTO: " + "\n\n\n" + "CREDITO: " + credit[i]);
+               // credito += credit[i];
+                System.out.println(credito);
+                //jTextArea.setText("INSERITI: " + "\n" + "COSTO: " + "\n\n\n" + "CREDITO: " + credito);
             }
-            jTextArea.setText("INSERITI: " + "\n" + "COSTO: " + "\n\n\n" + "CREDITO: " + credit);
 
-        }*/
+
+        }
 
 
         //jTextArea.repaint();
