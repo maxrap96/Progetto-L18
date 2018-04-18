73742d5192832d8eebb0b5c6e59487762fc6a34b
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-04-18 16:12:29 +0200

    Aggiunti pulsanti monete

--- src/Distributore/InterfacciaDistributore/TesterInterfaccia.java
+++ src/Distributore/InterfacciaDistributore/TesterInterfaccia.java
@@ -1,11 +1,13 @@
 package Distributore.InterfacciaDistributore;
 
 import Distributore.Distributore;
+import Distributore.Coins;
 
 public class TesterInterfaccia {
     public static void main(String[] args) {
-        Distributore distributore=new Distributore();
-        VendingMachine d = new VendingMachine(distributore);
+        Distributore distributore = new Distributore();
+        Coins coins = new Coins();
+        VendingMachine d = new VendingMachine(distributore, coins);
         d.setVisible(true);
     }
 }
