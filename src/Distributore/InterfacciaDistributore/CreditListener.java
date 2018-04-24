8a3b7e49bc1ac1cfe92acc2e6dcbfa33e6dbd3fa
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-04-24 16:22:28 +0200

    Modifiche qualità codice interfaccia

---
+++ src/Distributore/InterfacciaDistributore/CreditListener.java
@@ -0,0 +1,26 @@
+package Distributore.InterfacciaDistributore;
+
+import Distributore.Distributore;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CreditListener implements ActionListener{
+
+    private Distributore distributore;
+    private JTextArea textAreaL;
+    private double value;
+
+    public CreditListener(Distributore distributore, JTextArea display, double value) {
+        this.distributore = distributore;
+        this.textAreaL = display;
+        this.value = value;
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        String s = e.getActionCommand();
+        distributore.addCredit(value);
+        textAreaL.setText("\n\n\nCREDITO: " +  String.format("%.2f", distributore.getCredit()));
+    }
+}
