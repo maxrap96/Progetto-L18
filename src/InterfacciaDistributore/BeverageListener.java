71a4807b99e15ebf9f63343af578fd58fe62338e
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-04-25 09:16:18 +0200

    Timer display bevande implementato
    Dopo aver premuto il pulsante di una bevanda, il nome e il prezzo restano per 10 secondi, poi torna la schermata principale

--- src/InterfacciaDistributore/BeverageListener.java
+++ src/InterfacciaDistributore/BeverageListener.java
@@ -23,14 +23,14 @@ public class BeverageListener implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
         String s = e.getActionCommand();
-        Timer t = new Timer();
-      TimerTask tr = new TimerTask() {
+        Timer timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
           @Override
           public void run() {
-              System.out.println("c");
+              textAreaL.setText("     SCEGLIERE UNA BEVANDA");
           }
-      };
-        t.schedule(tr, 3000);
+        };
+        timer.schedule(timerTask, 10000);
         textAreaL.setText(distributoreL.getLabel(indexL).toUpperCase() + "\n" + "COSTO: "
                           + String.format("%.2f", distributoreL.getPrice("0" + indexL)));
         distributoreL.selectBeverage("0" + indexL);
