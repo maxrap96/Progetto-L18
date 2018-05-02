113a6a19a3c2cc666489c05211c8f621d44fdea5
Mladen <33460160+mladenmj@users.noreply.github.com>
2018-05-03 01:08:36 +0200

    Added Bar Chart. Link to server needed, still working on it...

---
+++ src/Server/BarChart.java
@@ -0,0 +1,42 @@
+package Server;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+
+public class BarChart extends Application {
+    final static String coins = "0.05";
+    final static String coins2 = "0.10";
+    final static String coins3 = "0.20";
+    final static String coin4 = "0.50";
+    final static String coins5 = "1";
+    final static String coins6 = "2";
+
+    @Override
+    public void start(Stage stage) {
+        stage.setTitle("Monete rimanenti:");
+        final NumberAxis xAxis = new NumberAxis();
+        final CategoryAxis yAxis = new CategoryAxis();
+        final javafx.scene.chart.BarChart<Number, String> bc =
+                new javafx.scene.chart.BarChart<Number, String>(xAxis, yAxis);
+        bc.setTitle("Coins");
+        xAxis.setLabel("Numero monete rimaste");
+        yAxis.setLabel("Tagli di monete [€]");
+
+        XYChart.Series series1 = new XYChart.Series();
+        series1.getData().add(new XYChart.Data(100, coins));
+        series1.getData().add(new XYChart.Data(176, coins2));
+        series1.getData().add(new XYChart.Data(100, coins3));
+        series1.getData().add(new XYChart.Data(200, coin4));
+        series1.getData().add(new XYChart.Data(50, coins5));
+        series1.getData().add(new XYChart.Data(300, coins6));
+
+        Scene scene = new Scene(bc, 800, 600);
+        bc.getData().addAll(series1);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/Server/TabsListener.java
+++ src/Server/TabsListener.java
@@ -1,5 +1,12 @@
 package Server;
 
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,18 +14,19 @@ import java.awt.event.ActionListener;
 
 public class TabsListener implements ActionListener {
     JPanel statsPanel;
-    JTabbedPane jTabListener;
+    JTabbedPane jTabs;
+    BarChart coinsChart;
 
     public TabsListener(JPanel infoPanel) {
         this.statsPanel = infoPanel;
-        jTabListener = new JTabbedPane();
+        jTabs = new JTabbedPane();
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         statsPanel.removeAll();
         createTabs();
-        statsPanel.add(jTabListener);
+        statsPanel.add(jTabs);
         statsPanel.validate();
         statsPanel.repaint();
     }
@@ -27,15 +35,18 @@ public class TabsListener implements ActionListener {
      * Funzione che crea le schede delle statistiche
      */
     private void createTabs() {
-        jTabListener.removeAll();
+        jTabs.removeAll();
 
         JPanel panel = makeTextPanel("Statistiche relative alle monete:");
-        jTabListener.addTab("Monete", panel);
+        jTabs.addTab("Monete", panel);
+
+        //createFirstTab();
+
         JComponent panel2 = makeTextPanel("Statistiche relative agli ingredienti:");
-        jTabListener.addTab("Ingredienti", panel2);
+        jTabs.addTab("Ingredienti", panel2);
         JComponent panel3 = makeTextPanel("Statistiche varie:");
         panel3.setPreferredSize(new Dimension(410, 50));
-        jTabListener.addTab("Miscellanea", panel3);
+        jTabs.addTab("Miscellanea", panel3);
     }
 
     // MJ: può essere tolta in futuro e sostituita con grafici delle statistiche.
@@ -48,4 +59,40 @@ public class TabsListener implements ActionListener {
         panel.add(filler);
         return panel;
     }
+
+
+    // MJ: work in progress su tabs con javaFX...
+
+    /*private void createFirstTab() {
+        JFXPanel panel = makeTextPanelNew("Statistiche relative alle monete:");
+        jTabs.addTab("Monete", panel);
+    }
+
+    private JFXPanel makeTextPanelNew(String text) {
+        JFXPanel panel = new JFXPanel();
+        panel.setPreferredSize(new Dimension(500, 800));
+        panel.setLayout(new GridLayout(1, 1));
+        Label label = new Label();
+
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                initFX(panel);
+            }
+        });
+
+        JPanel jp = new JPanel();
+        jp.setLayout(new BorderLayout());
+        jp.add(panel, BorderLayout.CENTER);
+
+        return panel;
+    }
+
+    private static void initFX(JFXPanel fxPanel) {
+        // This method is invoked on JavaFX thread
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.PLUM);
+
+        fxPanel.setScene(scene);
+    }*/
 }
