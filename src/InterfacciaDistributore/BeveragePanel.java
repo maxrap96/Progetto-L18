61770271e2395e758ff0f1b8e6d7aade8413cf17
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-05-03 11:20:14 +0200

    Sistemato problema dimensioni pulsanti bevande

--- src/InterfacciaDistributore/BeveragePanel.java
+++ src/InterfacciaDistributore/BeveragePanel.java
@@ -6,29 +6,31 @@ import javax.swing.*;
 import java.awt.*;
 
 public class BeveragePanel extends JPanel {
+    private final int NUMERO_PULSANTI_BEVANDE = 12;
     private Distributore distributorePanel;
     private int index = 1;
 
     public BeveragePanel(Distributore distributore) {
         this.distributorePanel = distributore;
         this.setLayout(new GridBagLayout());
-
-        this.setBackground(Color.RED);
+        this.setBackground(Color.BLACK);
         makeBeverageButton();
-
-
     }
 
     private void makeBeverageButton() {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        Dimension screenSize = kit.getScreenSize();
+        setSize(screenSize.width,screenSize.height);
+
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.BOTH;
-        c.insets = new Insets(50,40,50,40); // Spazio dai bordi
-        c.weightx = 1;  // Riempio la metà orizzontale dello spazio del bottone. Valori ammessi 0..1
-        c.weighty = 1;  // Riempio la metà verticale dello spazio del bottone. Valori ammessi 0..1
+        c.insets = new Insets(50,40,50,40);
+        c.weightx = 1;
+        c.weighty = 1;
         c.gridx = 0;
         c.gridy = 0;
 
-        for (int i = 0; i < 12; i++) {
+        for (int i = 0; i < NUMERO_PULSANTI_BEVANDE; i++) {
             if (c.gridx == 3) {
                 c.gridx = 0;
                 c.gridy++;
@@ -38,7 +40,9 @@ public class BeveragePanel extends JPanel {
             if (i < distributorePanel.getListSize()) {
                 index = i + 1;
                 button = new RoundRectButton(distributorePanel.getLabel(index));
-             // button.setPreferredSize(new Dimension(800, 100));
+                button.setMinimumSize(new Dimension(screenSize.width / 6, screenSize.height / 8));
+                button.setMaximumSize(new Dimension(screenSize.width / 6, screenSize.height / 8));
+                button.setPreferredSize(new Dimension(screenSize.width / 6, screenSize.height / 8));
                 button.setFont(new Font("", Font.BOLD, 15));
 
                // ResetDisplay resetDisplay = new ResetDisplay(display, sugarDisplay, distributorePanel);
@@ -47,6 +51,9 @@ public class BeveragePanel extends JPanel {
             else {
                 // Pulsante vuoto
                 button = new RoundRectButton("");
+                button.setMinimumSize(new Dimension(screenSize.width / 6, screenSize.height / 8));
+                button.setMaximumSize(new Dimension(screenSize.width / 6, screenSize.height / 8));
+                button.setPreferredSize(new Dimension(screenSize.width / 6, screenSize.height / 8));
                 //ResetDisplay resetDisplay = new ResetDisplay(display, sugarDisplay, distributore);
                 //button.addActionListener(new BeverageListener(distributore, display, index, resetDisplay));
             }
