99ac18e98151e5c2e5ae95cf1794a65031ac31b3
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-05-03 15:36:40 +0200

    Nuovo Frame

--- src/InterfacciaDistrubutoreFX/VendingMachine.java
+++ src/InterfacciaDistrubutoreFX/VendingMachine.java
@@ -1,16 +1,40 @@
 package InterfacciaDistrubutoreFX;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
+;import java.io.FileInputStream;
+
 
 public class VendingMachine extends Application {
 
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hot Drinks Vending Machine");
+
+        FileInputStream input = new FileInputStream("src/InterfacciaDistrubutoreFX/I.JPG");
+        Image image = new Image(input);
+        ImageView imageView = new ImageView(image);
+
+        GridPane beveragePane = new GridPane();
+        beveragePane.getChildren().add(imageView);
+
+        Scene scene = new Scene(beveragePane, 400, 200, Color.LIGHTGRAY);
+        primaryStage.setScene(scene);
+
+        primaryStage.show();
     }
 
-    @Override
-    public void start(Stage primaryStage) {
-
+    public static void main(String[] args) {
+        Application.launch(args);
     }
 }
