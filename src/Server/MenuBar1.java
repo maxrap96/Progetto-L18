17c3f11786c51611e89f2cc25169c60b49438fcd
AndreaLucenti <32876099+AndreaLucenti@users.noreply.github.com>
2018-05-03 15:46:53 +0200

    Aggiunta MenuBar
    Aggiunta Classe per la menubar

---
+++ src/Server/MenuBar1.java
@@ -0,0 +1,73 @@
+package Server;
+
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MenuBar1 extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX App");
+        ImageView ImmStats = new ImageView("file:src/Server/stats.jpg");
+        ImageView ImmMenu = new ImageView("file:src/Server/menu.png");
+        ImageView ImmHome = new ImageView("file:src/Server/home.png");
+        ImageView ImmSx = new ImageView("file:src/Server/FrecciaSx.png");
+        ImageView ImmDx = new ImageView("file:src/Server/FrecciaDx.png");
+
+        ImmHome.setFitHeight(20);
+        ImmHome.setFitWidth(20);
+        ImmStats.setFitHeight(20);
+        ImmStats.setFitWidth(20);
+        ImmMenu.setFitWidth(20);
+        ImmMenu.setFitHeight(20);
+        ImmDx.setFitWidth(20);
+        ImmDx.setFitHeight(20);
+        ImmSx.setFitWidth(20);
+        ImmSx.setFitHeight(20);
+
+        Menu sx = new Menu();
+        Menu dx = new Menu();
+        Menu home = new Menu();
+        Menu menu = new Menu("Menu");
+        Menu stats = new Menu("Stats");
+        stats.setGraphic(ImmStats);
+        menu.setGraphic(ImmMenu);
+        home.setGraphic(ImmHome);
+        sx.setGraphic(ImmSx);
+        dx.setGraphic(ImmDx);
+        MenuBar menuBar = new MenuBar();
+
+        menuBar.getMenus().add(sx);
+        menuBar.getMenus().add(dx);
+        menuBar.getMenus().add(home);
+        menuBar.getMenus().add(menu);
+        menuBar.getMenus().add(stats);
+
+
+
+        MenuItem dati = new MenuItem("Dati");
+        MenuItem statistiche = new MenuItem("General Stats");
+        MenuItem monete = new MenuItem("Money");
+        stats.getItems().add(dati);
+        stats.getItems().add(statistiche);
+        stats.getItems().add(monete);
+
+        VBox vBox = new VBox(menuBar);
+
+        Scene scene = new Scene(vBox, 960, 600);
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
