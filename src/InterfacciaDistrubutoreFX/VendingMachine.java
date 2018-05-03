bf67eb0e65cae38a7a1a60805aa3d9d5edc11372
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2018-05-03 14:12:03 +0200

    aggiunta pkg interfaccia fx

---
+++ src/InterfacciaDistrubutoreFX/VendingMachine.java
@@ -0,0 +1,16 @@
+package InterfacciaDistrubutoreFX;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class VendingMachine extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
