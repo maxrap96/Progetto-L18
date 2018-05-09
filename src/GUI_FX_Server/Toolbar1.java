606cfb4a29325d0f2c6aa9d23ee35f5254350fa5
AndreaLucenti <32876099+AndreaLucenti@users.noreply.github.com>
2018-05-09 21:05:15 +0200

    Commentato frecce
    Commentato frecce

--- src/GUI_FX_Server/Toolbar1.java
+++ src/GUI_FX_Server/Toolbar1.java
@@ -11,34 +11,36 @@ import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
+import javax.swing.*;
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Toolbar1 extends ToolBar {
-    Button sx = new Button();
-    Button dx = new Button();
+   // Button sx = new Button();
+   // Button dx = new Button();
     Button home = new Button();
     Button save = new Button("SAVE");
+
 
     public Toolbar1(Stage stage) {
         ImageView immStats = new ImageView(loadImage("src/ServerImages/stats.jpg"));
         ImageView immHome = new ImageView(loadImage("src/ServerImages/home.png"));
-        ImageView immSx = new ImageView(loadImage("src/ServerImages/FrecciaSx.png"));
-        ImageView immDx = new ImageView(loadImage("src/ServerImages/FrecciaDx.png"));
+     //   ImageView immSx = new ImageView(loadImage("src/ServerImages/FrecciaSx.png"));
+     //   ImageView immDx = new ImageView(loadImage("src/ServerImages/FrecciaDx.png"));
 
         immHome.setFitHeight(20);
         immHome.setFitWidth(20);
         immStats.setFitHeight(20);
         immStats.setFitWidth(20);
-        immDx.setFitWidth(20);
-        immDx.setFitHeight(20);
-        immSx.setFitWidth(20);
-        immSx.setFitHeight(20);
+//        immDx.setFitWidth(20);
+//        immDx.setFitHeight(20);
+//        immSx.setFitWidth(20);
+//        immSx.setFitHeight(20);
 
         home.setGraphic(immHome);
-        sx.setGraphic(immSx);
-        dx.setGraphic(immDx);
-        getItems().addAll(sx,dx,home,save);
+       // sx.setGraphic(immSx);
+       // dx.setGraphic(immDx);
+        getItems().addAll(home,save);
 
         Action(stage);
     }
