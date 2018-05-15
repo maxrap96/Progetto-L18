3d492eeb554dffce150e118ca9bfe66d6d191839
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-05-15 15:41:38 +0200

    Cancellazione GUI_Swing

--- src/GUI_Swing/TesterInterfaccia.java
+++
@@ -1,9 +0,0 @@
-package GUI_Swing;
-
-
-public class TesterInterfaccia {
-    public static void main(String[] args) {
-        VendingMachine d = new VendingMachine();
-        d.setVisible(true);
-    }
-}
