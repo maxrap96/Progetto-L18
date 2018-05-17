9767b9e74dd53aad36aaf6dbdc83cb1e6c05fd45
AndreaLucenti <32876099+AndreaLucenti@users.noreply.github.com>
2018-05-17 20:08:13 +0200

    Aggiunta possibilità di cambiare i valori nell menutable
    Aggiunta possibilità di cambiare i valori nell menutable

--- src/GUI_FX_Server/MenuTable.java
+++ src/GUI_FX_Server/MenuTable.java
@@ -3,10 +3,13 @@ package GUI_FX_Server;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TreeTableColumn;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -31,31 +34,120 @@ public class MenuTable extends TableView {
         }
 
         ObservableList<Tabella> data = FXCollections.observableArrayList();
-        for(int i = 0; i < tabella.length; i++) {
+        for(int i = 1; i < tabella.length; i++) {
             data.addAll(tabella[i]);
         }
 
-        //Aggiungo ad ogni colonna i valori contenuti nella classe menu
+        //Creo colonne aggiungo ad ogni colonna i valori contenuti nella classe tabella e rendo i valori modificabili
         TableColumn id = new TableColumn("ID");
         id.setCellValueFactory(new PropertyValueFactory<Tabella, String>("id"));
+        id.setCellFactory(TextFieldTableCell.forTableColumn());
+        id.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setId(event.getNewValue());
+            }
+        });
+
         TableColumn tipo = new TableColumn("Tipo");
         tipo.setCellValueFactory(new PropertyValueFactory<Tabella, String>("tipo"));
+        tipo.setCellFactory(TextFieldTableCell.forTableColumn());
+        tipo.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setTipo(event.getNewValue());
+            }
+        });
+
         TableColumn nome = new TableColumn("Nome");
         nome.setCellValueFactory(new PropertyValueFactory<Tabella, String>("nome"));
+        nome.setCellFactory(TextFieldTableCell.forTableColumn());
+        nome.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setNome(event.getNewValue());
+            }
+        });
+
         TableColumn costo = new TableColumn("Costo");
         costo.setCellValueFactory(new PropertyValueFactory<Tabella, String>("costo"));
+        costo.setCellFactory(TextFieldTableCell.forTableColumn());
+        costo.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setCosto(event.getNewValue());
+            }
+        });
+
         TableColumn Q_max = new TableColumn("Q_max");
         Q_max.setCellValueFactory(new PropertyValueFactory<Tabella, String>("q_max"));
+        Q_max.setCellFactory(TextFieldTableCell.forTableColumn());
+        Q_max.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setQ_max(event.getNewValue());
+            }
+        });
+
         TableColumn temp = new TableColumn("Temp");
         temp.setCellValueFactory(new PropertyValueFactory<Tabella, String>("temp"));
+        temp.setCellFactory(TextFieldTableCell.forTableColumn());
+        temp.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setTemp(event.getNewValue());
+            }
+        });
+
         TableColumn dose = new TableColumn("Dose");
         dose.setCellValueFactory(new PropertyValueFactory<Tabella, String>("dose"));
+        dose.setCellFactory(TextFieldTableCell.forTableColumn());
+        dose.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setDose(event.getNewValue());
+            }
+        });
+
         TableColumn latte = new TableColumn("Latte");
         latte.setCellValueFactory(new PropertyValueFactory<Tabella, String>("latte"));
+        latte.setCellFactory(TextFieldTableCell.forTableColumn());
+        latte.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setLatte(event.getNewValue());
+            }
+        });
+
         TableColumn acqua = new TableColumn("Acqua");
         acqua.setCellValueFactory(new PropertyValueFactory<Tabella, String>("acqua"));
+        acqua.setCellFactory(TextFieldTableCell.forTableColumn());
+        acqua.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setAcqua(event.getNewValue());
+            }
+        });
+
         TableColumn vodka = new TableColumn("vodka");
         vodka.setCellValueFactory(new PropertyValueFactory<Tabella, String>("vodka"));
+        vodka.setCellFactory(TextFieldTableCell.forTableColumn());
+        vodka.setOnEditCommit( new EventHandler<TableColumn.CellEditEvent<Tabella, String>>() {
+            public void handle ( TableColumn.CellEditEvent<Tabella, String> event) {
+                ((Tabella) event.getTableView().getItems().get(
+                        event.getTablePosition().getRow())
+                ).setVodka(event.getNewValue());
+            }
+        });
 
         tableView.setItems(data);
         tableView.setEditable(false);
@@ -63,13 +155,11 @@ public class MenuTable extends TableView {
 
         tableView.getColumns().addAll(id, tipo, nome, costo, Q_max, temp, dose, latte, acqua, vodka);
 
-        //Creazione toolbar e aggiunta tasto Save ad essa
-        Button save = new Button("Save");
+        tableView.setEditable(true);
 
         vBox.getChildren().addAll(tableView);
-        vBox.setFillWidth(true);;
+        vBox.setFillWidth(true);
         VBox.setVgrow(tableView, Priority.ALWAYS);
-
         vBox.prefHeightProperty().bind(stage.heightProperty());
         vBox.prefWidthProperty().bind(stage.widthProperty());
     }
@@ -104,88 +194,85 @@ public class MenuTable extends TableView {
             this.vodka = new SimpleStringProperty(dati[9]);
         }
 
-
-        public String getId() {
-            return id.get();
-        }
-
         public SimpleStringProperty idProperty() {
             return id;
-        }
-
-        public String getTipo() {
-            return tipo.get();
         }
 
         public SimpleStringProperty tipoProperty() {
             return tipo;
         }
 
-        public String getNome() {
-            return nome.get();
-        }
-
         public SimpleStringProperty nomeProperty() {
             return nome;
-        }
-
-        public String getCosto() {
-            return costo.get();
         }
 
         public SimpleStringProperty costoProperty() {
             return costo;
         }
 
-        public String getQ_max() {
-            return q_max.get();
-        }
-
         public SimpleStringProperty q_maxProperty() {
             return q_max;
-        }
-
-        public String getTemp() {
-            return temp.get();
         }
 
         public SimpleStringProperty tempProperty() {
             return temp;
         }
 
-        public String getDose() {
-            return dose.get();
-        }
-
         public SimpleStringProperty doseProperty() {
             return dose;
-        }
-
-        public String getLatte() {
-            return latte.get();
         }
 
         public SimpleStringProperty latteProperty() {
             return latte;
         }
 
-        public String getAcqua() {
-            return acqua.get();
-        }
-
         public SimpleStringProperty acquaProperty() {
             return acqua;
-        }
-
-        public String getVodka() {
-            return vodka.get();
         }
 
         public SimpleStringProperty vodkaProperty() {
             return vodka;
         }
 
+        public void setId(String id) {
+            this.id.set(id);
+        }
 
+        public void setTipo(String tipo) {
+            this.tipo.set(tipo);
+        }
+
+        public void setNome(String nome) {
+            this.nome.set(nome);
+        }
+
+        public void setCosto(String costo) {
+            this.costo.set(costo);
+        }
+
+        public void setQ_max(String q_max) {
+            this.q_max.set(q_max);
+        }
+
+        public void setTemp(String temp) {
+            this.temp.set(temp);
+        }
+
+        public void setDose(String dose) {
+            this.dose.set(dose);
+        }
+
+        public void setLatte(String latte) {
+            this.latte.set(latte);
+        }
+
+        public void setAcqua(String acqua) {
+            this.acqua.set(acqua);
+        }
+
+        public void setVodka(String vodka) {
+            this.vodka.set(vodka);
+        }
     }
 
     public VBox getvBox() {
