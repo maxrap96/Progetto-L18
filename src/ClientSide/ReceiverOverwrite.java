5f5b970232afbe0383f4862ec940e736b1e6357a
DarioMaestri <32876188+Dareus1996@users.noreply.github.com>
2018-06-01 14:29:05 +0200

    Added MENU_PATH in ReceiverOverwrite

--- src/ClientSide/ReceiverOverwrite.java
+++ src/ClientSide/ReceiverOverwrite.java
@@ -7,18 +7,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class ReceiverOverwrite implements TextPathFiles {
-    private Data menuData = new Data("src/File_Testo/menu.txt");
+    private Data menuData = new Data(MENU_PATH);
 
     protected void overwriteFileReceiver(ArrayList<String> arrayFromCommand) {
         try {
-            saveFileFromCommand(arrayFromCommand);
-            System.out.println("File Menu Saved");
+            saveFileReceived(arrayFromCommand);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void saveFileFromCommand(ArrayList<String> arrayFromCommand) throws IOException {
+    private void saveFileReceived(ArrayList<String> arrayFromCommand) throws IOException {
         menuData.saveFileFromCommand(arrayFromCommand);
     }
 }
