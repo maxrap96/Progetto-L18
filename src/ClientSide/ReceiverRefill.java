7b8ec7a54b1d4ea661eb19c9934162ebf20147de
HexRebuilt <32871843+HexRebuilt@users.noreply.github.com>
2018-06-04 12:15:26 +0200

    refactor

--- src/ClientSide/ReceiverRefill.java
+++ src/ClientSide/ReceiverRefill.java
@@ -20,7 +20,7 @@ public class ReceiverRefill implements MaxValue, TextPathFiles, CoinsNumbers {
     /**
      * funzione per refillare gli items e scrivere su file le quantità riempite
      */
-    public void RefillItems(){
+    protected void RefillItems(){
         ArrayList<String[]> oldData = beverageFile.readFile();
         try {
                 beverageFile.overwriteFile(String.valueOf(oldData.get(0)), (ITEMS[0] + MILK_MAX));
@@ -37,30 +37,30 @@ public class ReceiverRefill implements MaxValue, TextPathFiles, CoinsNumbers {
      * funzione per riempire le bevande basandosi sulla quantià massima presente nel menu
      */
 
-    public void RefillBeverage(){
+    protected void RefillBeverage(){
         ArrayList<String[]> oldData = beverageFile.readFile();
+        try {
         for (int i = 0; i < oldData.size(); i++ ){
             if (oldData.get(i)[0].startsWith("0")){ //significa che è+ una entrata del menu
                 String currentId = oldData.get(i)[0];
                 for (int j = 0; i < menu.size(); i++){
                     if (menu.get(j)[0].equals(currentId)){
                         double currentMaxQuantity = parseDouble(menu.get(j)[4]);
-                        try {
-                            beverageFile.overwriteFile(String.valueOf(oldData.get(i)), (currentId + currentMaxQuantity));
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        beverageFile.overwriteFile(String.valueOf(oldData.get(i)), (currentId + currentMaxQuantity));
                     }
                 }
 
             }
+        }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     /**
      * funzione per refillare i coins
      */
-    public void RefillCoins(){
+    protected void RefillCoins(){
         String moneyCount = String.valueOf(MONEY_COUNT[0] + "\t" + MONEY_COUNT[1] + "\t"  + MONEY_COUNT[2] + "\t"
                 +MONEY_COUNT[3] + "\t" + MONEY_COUNT[4] + "\tServer refill");
         coinsFile.writeData(moneyCount);
