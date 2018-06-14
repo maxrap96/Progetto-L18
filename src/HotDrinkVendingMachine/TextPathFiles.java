5f16ae6475b60a5653820ad027ee8029fa6c9a6b
maxrap96 <37112445+maxrap96@users.noreply.github.com>
2018-06-14 15:38:32 +0200

    Created TextFiles folder out of src

--- src/HotDrinkVendingMachine/TextPathFiles.java
+++ src/HotDrinkVendingMachine/TextPathFiles.java
@@ -1,9 +1,9 @@
 package HotDrinkVendingMachine;
 
 public interface TextPathFiles {
-    String STATS_PATH = "src/TextFiles/Stats.txt";
-    String DATA_PATH = "src/TextFiles/Data.txt";
-    String MENU_PATH = "src/TextFiles/Menu.txt";
-    String COINS_PATH = "src/TextFiles/Coins.txt";
-    String KEY_PATH = "src/TextFiles/KeyData.txt";
+    String STATS_PATH = "TextFiles/Stats.txt";
+    String DATA_PATH = "TextFiles/Data.txt";
+    String MENU_PATH = "TextFiles/Menu.txt";
+    String COINS_PATH = "TextFiles/Coins.txt";
+    String KEY_PATH = "TextFiles/KeyData.txt";
 }
